382ab5105a55eadd10591c5c27c65d4e5ae0d179
Ximena <moreraximena36@gmail.com>
2024-11-06 19:57:25 -0500

    initial commit

---
+++ database/migrations/2024_11_07_002916_create_tickets_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->enum('status', ['abierto', 'en progreso', 'finalizado'])->default('abierto');
+            $table->integer('created_by');  // Usuario que creó el ticket (administrador)
+            $table->integer('assigned_to')->nullable();  // Usuario asignado (soporte)
+            $table->timestamps();
+
+            // Relaciones con la tabla users
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('assigned_to')->references('id')->on('users')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
